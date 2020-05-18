77df35220714750aa465a620288ff8003d8571f3
Tan Pinxi <h1110104@nushigh.edu.sg>
2020-05-19 00:30:53 +0800

    Allow multi-session cookie

--- App/Focus.js
+++ App/Focus.js
@@ -21,64 +21,90 @@ var monitoredSites = ["facebook.com", "instagram.com", "youtube.com", "reddit.co
 (function() {
     'use strict';
 
-    $(document).ready(() => {
-        //console.log(GM.listValues());
+    $(window).on("load", () => {
+        console.log("Ready/Loaded");
+        isMonitored = updateCookie();
+        if (!isMonitored){
+            console.log("Clean Site");
+        }
     });
 
+    $(window).on("unload", () => {
+        console.log("Unloaded");
+        clearCookie();
+    });
 
-    $(document).on('mousemove', function() {
+    $(document).on('mousemove', () => {
         if (isMonitored){
             clearTimeout(timeout);
 
             timeout = setTimeout(function() {
-                console.log('Mouse idle for 10 sec');
+                console.log('Mouse idle for 2 sec');
                 updateCookie();
-            }, 10000);
+            }, 2000);
         }
     });
 
-    $(window).on('hashchange', function(e){
-        isMonitored = updateCookie()
-        if (!isMonitored){
-            console.log("Clean Site");
-        }
+    $(window).on('hashchange', () => {
     }).trigger('hashchange');
 
     function updateCookie() {
 
-        var url = window.location.href;
-        var updated = false;
+        const site = getSite();
+
+        if (site.localeCompare("") != 0){
+
+            GM.getValue(site, []).then(x => {
+
+                var timeNow = new Date($.now());
+
+                if (x.length == undefined || x.length == 0){
+                    console.log("New site accessed at " + timeNow);
+                    GM.setValue(currentSite, [timeNow]);
+                }
+                else if (x.length == 1){
+                    console.log("Site last checked at " + timeNow);
+                    x.push(timeNow);
+                    GM.setValue(site, x);
+                }
+                else if (x.length == 2){
+                    console.log("Site last check updated to " + timeNow);
+                    x[1] = timeNow;
+                    GM.setValue(site, x);
+                }
+                else{
+                    console.log("Cookie has length " + x.length);
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+
+    function clearCookie(){
+        const site = getSite();
+
+        GM.getValue(site, []).then(x => {
+            if (x.length == 2){
+                const diff = x[1] - x[0];
+                storeUsage(site, diff);
+            }
+        });
+    }
+
+    function storeUsage(site, diff){
+    }
+
+    function getSite(){
+
+        const url = window.location.href;
 
         for (var i = 0; i < monitoredSites.length; i++){
             if (url.includes(monitoredSites[i])){
-
-                updated = true;
-
-                GM.getValue(monitoredSites[i], []).then(x => {
-
-                    if (x.length == 0){
-                        console.log("New Monitored Site");
-                        var accessTime = $("#datetime").val();
-                        GM.setValue(monitoredSites[i], [accessTime]);
-                    }
-                    else {
-                        console.log("Returning Monitored Site");
-                        var lastTouchTime = $("#datetime").val();
-
-                        if (x.length == 1){
-                            GM.setValue(monitoredSites[i], x.push(lastTouchTime));
-                        }
-                        else if (x.length == 2){
-                            x[1] = lastTouchTime;
-                            GM.setValue(monitoredSites[i], x);
-                        }
-                    }
-                });
-                break;
+                return monitoredSites[i];
             }
         }
-
-        return updated;
+        return "";
     }
 
 })();
